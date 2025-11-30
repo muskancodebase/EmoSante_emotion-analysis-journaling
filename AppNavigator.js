691e3bcc72750dee2baa1dae8801e01e6e367d72
Silver_Umer <142928131+SilverUmer21@users.noreply.github.com>
2025-11-30 16:24:45 +0500

    App navigation added

---
+++ AppNavigator.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import JournalListScreen from '../screens/JournalListScreen';
+import AddJournalScreen from '../screens/AddJournalScreen';
+import EditJournalScreen from '../screens/EditJournalScreen';
+import AudioToTextScreen from '../screens/AudioToTextScreen';
+import EmotionTagScreen from '../screens/EmotionTagScreen';
+import MoodPaletteScreen from '../screens/MoodPaletteScreen';
+import StreakScreen from '../screens/StreakScreen';
+import SearchEntriesScreen from '../screens/SearchEntriesScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import ResetPasswordScreen from '../screens/ResetPasswordScreen';
+import ExportEntriesScreen from '../screens/ExportEntriesScreen';
+import theme from '../theme';
+
+const { colors } = theme;
+
+const Stack = createNativeStackNavigator();
+
+const navigationTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: colors.background,
+    primary: colors.primary,
+    card: colors.surface,
+    border: colors.borderSoft,
+    text: colors.text,
+  },
+};
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.primary,
+          },
+          headerTintColor: colors.textOnPrimary,
+          headerTitleStyle: {
+            fontWeight: '600',
+          },
+          contentStyle: {
+            backgroundColor: colors.background,
+          },
+          headerShadowVisible: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ResetPassword"
+          component={ResetPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="JournalList"
+          component={JournalListScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AddJournal"
+          component={AddJournalScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EditJournal"
+          component={EditJournalScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AudioToText"
+          component={AudioToTextScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EmotionTag"
+          component={EmotionTagScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MoodPalette"
+          component={MoodPaletteScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Streak"
+          component={StreakScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SearchEntries"
+          component={SearchEntriesScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: 'Settings', headerShown: false }}
+        />
+        <Stack.Screen
+          name="ExportEntries"
+          component={ExportEntriesScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
